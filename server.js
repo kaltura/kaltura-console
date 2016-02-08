f5161a5671b7e902dcb1f40f0d6aca06aee270e2
Ubuntu <ubuntu@ip-10-0-0-252.us-west-2.compute.internal>
2016-02-08 16:05:39 +0000

    start building server

---
+++ server.js
@@ -0,0 +1,15 @@
+var Express = require('express'),
+    App = Express();
+var Swagger = require('./static/swagger.json');
+var LucyConsole = require('lucy-console');
+
+var console = new LucyConsole({
+  swagger: Swagger,
+  cssIncludes: ['/css/bootstrap.css', '/css/styles.css'],
+  disableAutorefresh: true,
+  development: process.env.DEVELOPMENT ? true : false,
+})
+
+App.use(Express.static(__dirname + '/static'));
+App.use(console.router);
+App.listen(3000);
